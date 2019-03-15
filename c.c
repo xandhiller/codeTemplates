9152c94ecb3c3b5be03919cc0d646821569d7c90
Alex Hiller <xandhiller@gmail.com>
2019-03-15 13:07:59 +1100

    Repo init.

---
+++ c.c
@@ -0,0 +1,28 @@
+/*
+ * Author: Alex Hiller
+ * Year: 2019
+ *
+ * Program Description: 
+ *    <++>
+ *
+ */
+
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+
+
+/* #define <++> */
+
+
+/* enum {}; */
+
+
+int
+main (int argc, char *argv[]) {
+    <++>
+
+    return 0;
+}
